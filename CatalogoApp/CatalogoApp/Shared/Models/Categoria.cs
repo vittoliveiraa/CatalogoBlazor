6e1efd7201acf2d218a608000eb48589b92a4e1d
Victor Oliveira <vittoliveiraa@live.com>
2023-01-12 08:51:41 -0300

    Criado componente de  criação e edição de categoria

--- CatalogoApp/CatalogoApp/Shared/Models/Categoria.cs
+++ CatalogoApp/CatalogoApp/Shared/Models/Categoria.cs
@@ -4,10 +4,14 @@ namespace CatalogoApp.Shared.Models
 {
     public class Categoria
     {
-        [MaxLength(100)]
+        
         public int CategoriaId { get; set; }
-        [MaxLength(200)]
+
+        [Required(ErrorMessage = "O nome da Categoria é obrigatório")]
+        [MaxLength(100)]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "A descrição da categoria é obrigatória")]
+        [MaxLength(200)]
         public string DescricaoCategoria { get; set; }
 
         public ICollection<Produto> Produtos { get; set; }
